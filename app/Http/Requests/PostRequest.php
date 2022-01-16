ddd0b241eaf8c6fed3dec1e6c2c894d7c1105fca
jindo takumi <92016292+jtakumi@users.noreply.github.com>
2022-01-16 19:33:47 +0900

    Update PostRequest.php
    body size modified

--- app/Http/Requests/PostRequest.php
+++ app/Http/Requests/PostRequest.php
@@ -11,7 +11,7 @@ class PostRequest extends FormRequest
     {
         return [
             'post.title' => 'required|string|max:100',
-            'post.body' => 'required|string|max:4000',
+            'post.body' => 'required|string|max:20000',
         ];
     }
 }
