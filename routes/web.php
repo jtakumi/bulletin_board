7656ffba2a42611cd544ff5ffa629242a3c00032
jtakumi <takumi87jn@gmail.com>
2022-03-10 21:50:33 +0900

    procfile

--- routes/web.php
+++ routes/web.php
@@ -1,26 +1,6 @@
 <?php
 use App\Http\Controllers\LoginWithGoogleController;
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-#保存
-/*Route::get('/','PostController@index');
-Route::get('/posts/create','PostController@create');
-Route::post('/posts','PostController@store');
-Route::get('/posts/{post}','PostController@show');
-Route::put('/posts/{post}','PostController@update');
-Route::get('/posts/{post}/edit','PostController@edit');
-Route::delete('/posts/{post}','PostController@delete');
-Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');*/
 //google login function
 Route::get('/login/google','LoginWithGoogleController@getGoogleRedirect');
 Route::get('/login/google/callback','LoginWithGoogleController@loginGoogleCallback');
